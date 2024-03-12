6b194ce338e87c1a5fbf4c111a23d3f3d60ba6d0
Trong-khanh <tltkhanh1501@gmail.com>
2024-03-12 23:19:04 +0700

    JWT Authentication And Authorization

--- ManagementSchool/Controllers/AuthenticateController.cs
+++ ManagementSchool/Controllers/AuthenticateController.cs
@@ -1,7 +1,11 @@
-
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using ManagementSchool.Models.Login;
 using ManagementSchool.Models.SignUp;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using User.ManagementSchool.Service.Models;
 using User.ManagementSchool.Service.Service;
 
@@ -13,13 +17,15 @@ public class AuthenticateController: ControllerBase
     private readonly UserManager<IdentityUser> _userManager;
     private readonly RoleManager<IdentityRole> _roleManager;
     private readonly IEmailService _emailService;
+    private readonly IConfiguration _configuration;
     
     public AuthenticateController(UserManager<IdentityUser> userManager,
-        RoleManager< IdentityRole> roleManager, IEmailService emailService)
+        RoleManager< IdentityRole> roleManager, IEmailService emailService, IConfiguration configuration)
     {
         _roleManager = roleManager;
         _userManager = userManager;
         _emailService = emailService;
+        _configuration = configuration;
     }
     
     [HttpPost]
@@ -67,14 +73,14 @@ public class AuthenticateController: ControllerBase
         }
     }
 
-    [HttpGet("confirmEmail")]
+    [HttpGet("ConfirmEmail")]
     public async Task<IActionResult> ConfirmEmail(string token, string email)
     {
         var user = await _userManager.FindByEmailAsync(email);
         if (user != null)
         {
           var result = await _userManager.ConfirmEmailAsync(user, token);
-          if (!result.Succeeded)
+          if (result.Succeeded)
           {
               return StatusCode(StatusCodes.Status200OK,
                   new Response { Status ="Success", Message = "Email Verified Successfully" });
@@ -82,5 +88,46 @@ public class AuthenticateController: ControllerBase
         }
         return StatusCode(StatusCodes.Status500InternalServerError,
             new Response{Status = "Error", Message = "This User Doesnot Exist"});
+    }
+
+    [HttpPost]
+    [Route("Login")]
+    public async Task<IActionResult> Login([FromBody] LoginUser loginUser)
+    {
+        var user = await _userManager.FindByNameAsync(loginUser.UserName);
+        if (user != null && await _userManager.CheckPasswordAsync(user,loginUser.Password))
+        {
+            var authClaims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            };
+            var userRoles = await _userManager.GetRolesAsync(user);
+            foreach (var role in userRoles)
+            {
+                authClaims.Add(new Claim(ClaimTypes.Role,role));
+            }
+
+            var jwtToken = GetToken(authClaims);
+            return Ok(new
+            {
+                token = new JwtSecurityTokenHandler().WriteToken(jwtToken),
+                expiration = jwtToken.ValidTo
+            });
+        }
+        return Unauthorized();
+    }
+
+    private JwtSecurityToken GetToken(List<Claim> authClaims)
+    {
+        var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+        
+        var token = new JwtSecurityToken(
+        issuer: _configuration["JWT:ValidIssuer"],
+        audience: _configuration["JWT:ValidAudience"],
+        expires: DateTime.Now.AddHours(1),
+        claims: authClaims,
+        signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256));
+        return token;
     }
 }
---
+++ ManagementSchool/Models/Authentication/Login/LoginUser.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ManagementSchool.Models.Login;
+
+public class LoginUser
+{
+    [Required(ErrorMessage = "User Name is required")]
+    public string UserName { get; set; }
+    [Required(ErrorMessage = "Password is required")]
+    public string Password { get; set; }
+}
