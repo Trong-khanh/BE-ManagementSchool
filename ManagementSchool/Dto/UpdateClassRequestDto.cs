9076251a384f771bb8f9897d7f1cf07a9b36b1da
Trong khanh <tltkhanh1501@gmail.com>
2024-11-16 00:32:03 +0700

    Feat: Next Class

--- ManagementSchool/Dto/UpdateClassRequestDto.cs
+++ ManagementSchool/Dto/UpdateClassRequestDto.cs
@@ -2,8 +2,8 @@ namespace ManagementSchool.Dto;
 
 public class UpdateClassRequestDto
 {
-    public string CurrentAcademicYear { get; set; } // Năm học hiện tại
-    public string CurrentClassName { get; set; } // Tên lớp hiện tại
-    public string NewAcademicYear { get; set; } // Năm học mới
-    public string NewClassName { get; set; } // Tên lớp mới
+    public string CurrentAcademicYear { get; set; } 
+    public string CurrentClassName { get; set; } 
+    public string NewAcademicYear { get; set; }
+    public string NewClassName { get; set; } 
 }
